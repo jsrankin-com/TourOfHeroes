e3fce543b4dee328bb517a7bf5b8de59a676661e
jsrankin-com <admin@jsrankin.com>
2022-07-03 02:25:15 -0400

    You added the Angular router to navigate among different components You turned the AppComponent into a navigation shell with <a> links and a <router-outlet> You configured the router in an AppRoutingModule You defined routes, a redirect route, and a parameterized route You used the routerLink directive in anchor elements You refactored a tightly-coupled master/detail view into a routed detail view You used router link parameters to navigate to the detail view of a user-selected hero You shared the HeroService among multiple components

--- angular-tour-of-heroes/angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-
-import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
-import { MessagesComponent } from './components/messages/messages.component'; // <-- NgModel lives here
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
